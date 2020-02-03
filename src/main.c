3a520ddd585e78a325ce82aca2f7657e35d7521f
Carlos F. Velasquez <cfvelasquez095@gmail.com>
2020-02-03 15:29:07 -0600

    [WIP] Rendering HUD, Level and Spaceship with movement.

--- src/main.c
+++ src/main.c
@@ -5,34 +5,16 @@
 
 int main() {
     clear_screen();
-    set_color(WHITE, BLACK);
-    set_cursor(29, 15);
-    /* put_char('H');
-    put_char('e');
-    put_char('l');
-    put_char('l');
-    put_char('o');
-    put_char('!'); */
-    puts("Hello!");
-    
-    uint8_t f, b;
-    get_color(&f, &b);
-    
-    set_color(RED, BLACK);
-    puts("\x1\x2");
-    set_color(f, b);
-    
+
+    init_game();
+    render_spaceship();    
     keypad_init();
     while (1) {
-        uint8_t k = keypad_getkey();
-        set_cursor(2, 1);
-        put_char(TO_STR(k & 0xf));
-        set_cursor(3, 1);
-        put_char(TO_STR((k & 0xf0) >> 4));
-        if (k != 0) {
-            set_cursor(10, 15);
-            puts("Key press");
-        }
+        spaceship_logic(keypad_getkey());
+        // enemy_logic();
+        // spaceship_collision();
+        delay_ms(100);
+        render_game();
     }
 
     return 0;
